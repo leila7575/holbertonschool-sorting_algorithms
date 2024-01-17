c041f9523d22e8db59332ceeb465489afc5d136b
leila7575 <leila.louajri@gmail.com>
2024-01-17 20:53:58 +0100

    fixed betty coding style mistakes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,29 +8,29 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t step, i;
-    int temp;
-    int swapped;
+	size_t step, i;
+	int temp;
+	int swapped;
 
-    for (step = 0; step < size - 1; step ++)
-    {
-        swapped = 0;
+	for (step = 0; step < size - 1; step++)
+	{
+		swapped = 0;
 
-        for (i = 0; i < size - step -1; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                temp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = temp;
+		for (i = 0; i < size - step - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
 
-                swapped = 1;
+				swapped = 1;
 
-                print_array(array, size);
-            }       
-        }
+				print_array(array, size);
+			}
+		}
 
-        if (swapped == 0)
-            break;
-    }
+		if (swapped == 0)
+			break;
+	}
 }
