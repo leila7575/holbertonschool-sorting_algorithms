b0f0b2e810a2411f0e41deac2078a5997282f5f6
leila7575 <leila.louajri@gmail.com>
2024-01-19 16:49:28 +0100

    fixed betty coding style mistakes

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,17 +1,17 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts a doubly linked list using the Insertion sort algorithm
+ * insertion_sort_list - sorts using the Insertion sort algorithm
  *@list: pointer to head of doubly linked list
  */
 
- void insertion_sort_list(listint_t **list)
+void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *prev, *temp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	
+
 	current = (*list)->next;
 
 	while (current != NULL)
